9ddeb25e46cd71576f491319757f1e68d0c9abc0
alejandro jorge <alejorpas2@alu.edu.gva.es>
2023-02-08 18:07:36 +0100

    comm

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -2,12 +2,14 @@ package prueba1;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main (String[] args) {
 		System.out.println("Hola mundo java-git");
-		System.out.println("Hola mundo2 java-git");
-		System.out.println("Hola mundo3 java-git");
-		System.out.println("Pruebas Git. Se añade Cambio 1");
 	}
+
 }
 
 
